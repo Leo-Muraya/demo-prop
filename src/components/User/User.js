1f649498d144c2035f2b219d16890003a86b6aad
Leguminous <murayaleo911@gmail.com>
2024-11-05 11:41:06 +0300

    another one

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,49 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './User.module.css';
+import React from 'react'
+import styles from './User.module.css'
 
-function Usrs({name, age, isAdmin}){
-  const [name, setName] = useState("Mike")
-
-  const users = [
-    {id:1, name: "John Doe", location: "Nairobi"}
-  ]
+export default function User({name,location}) {
+  
+  return (
+    <div className={styles.User}>
+      <ul>
+        <li>{name} {location}</li>
+      </ul>
+    </div>
+  )
 }
 
-const User = () => (
-  <div className={styles.User}>
-
-    function handleOnChange(e){
-      console.log(e.target.value)
-    }
-
-    functtion handleSubmit(e){
-      setUsername(e.target.username.value);
-      setName(e.target.name.value);
-      setLocation(e.target.location.value);
-      setEmail(e.target.email.value);
-
-
-    }
-    
-    {/*inClick onSubmit on Change*/}
-    <form onSubmit={handleSubmit}>
-      <input placeholder='username' id="username"/>
-      <input placeholder='name' id="name" type='string'/>
-      <input placeholder='location' id="location" type='string'/>
-      <input placeholder='email' id="email" type='email'/>
-
-      <button type='submit'>Click Me!</button>
-
-
-      
-    </form>
-    <button onClick={handleClick}>Click Me!</button>
-  </div>
-);
-
-User.propTypes = {};
-
-User.defaultProps = {};
-
-export default User;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,47 +1,68 @@
-import React ,{useState} from 'react';
-import PropTypes from 'prop-types';
+import React,{useState, useEffect} from 'react';
 import styles from './Users.module.css';
-
-function Users({name, age, isAdmin}){
-  const [email,setEmail] =useState("")
-  const [location,setLocation] =useState("")
-  const [uname,setUname] =useState("")
-  const [username,setUsername] =useState("")
+import User from '../User/User';
 
 
-  const users = [
-    {id:1, name:"John Doe", loction:"Nairobi"},
-    {id:2, name:"Jane Doe", loction:"Kisumu"},
-    {id:3, name:"Steve Jobs", loction:"Mombasa"}
-  ]
-  
-  const userInfo = users.map((userObj)) => {
+function Users({ name, age, isAdmin }) {
+  const [email,setEmail] = useState("")
+  const [location,setLocation] = useState()
+  const [uname,setUname] = useState()
+  const [username,setUsername] = useState()
+  const [users,setUsers] =useState()
 
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+    .then((r) => r.json())
+    .then ((data)=> {
+      setUsers(data)
+    })
+  },[])
+
+  console.log(users)
+
+  // const userInfo = users.map((userObj) => {
+  //   return <User key={userObj.id} name={userObj.name} location={userObj.location} />
+  // })
+
+  function handleClick() {
+    alert("button clicked")
   }
 
-  function handleClick(){
-    alert("Button clicked")
-  }
 
+  function handleSubmit(e){
+    e.preventDefault()
+    const userObj = {
+      username:username,
+      name:uname,
+      email:email,
+      location:location
+    }
+    console.log(userObj)
+  }
+ 
   return (
-    <div className = {styles.Users}>
+    <div className={styles.Users}>
 
       <ul>
-        {isAdmin ? <li>{name} {age}</li> : <li>Current User is not admin</li>}
+        {isAdmin ? <li>{name} {age}</li> : <li>current user is not Admin</li>}
+
       </ul>
+      {/* {userInfo} */}
+
+      {/* onClick onSubmit onChange */}
+      <form onSubmit={handleSubmit}>
+        <input placeholder='username' id="username" onChange={(e)=> setUsername(e.target.value)} />
+        <input placeholder='uname' id="uname" type='string' onChange={(e)=> setUname(e.target.value)} />
+        <input placeholder='location' id="location" type='string' onChange={(e)=> setLocation(e.target.value)} />
+        <input placeholder='email' id="email" type='email' onChange={(e)=> setEmail(e.target.value)}/>
+
+        <button type="submit" >Submit</button>
+      </form>
+
     </div>
   )
-  
-}
+};
 
-const Users = () => (
-  <div className={styles.Users}>
-    Users Component
-  </div>
-);
-
-Users.propTypes = {};
-
-Users.defaultProps = {};
 
 export default Users;
